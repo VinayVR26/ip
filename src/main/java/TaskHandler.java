3e3bdc4f7efcef15b66f5f7f3e279850c6278ac2
VinayVR26 <133363378+VinayVR26@users.noreply.github.com>
2026-01-30 13:35:57 +0800

    Completed Level 3

--- src/main/java/TaskHandler.java
+++ src/main/java/TaskHandler.java
@@ -13,18 +13,38 @@ public class TaskHandler {
         }
     }
 
-    public static void displayUserDataArray(String[] userDataArray, int numberOfItems) {
+    public static int getTaskMarkNumber(String userInput) {
+        String number = "0";
+        int userInputNumber = userInput.length();
+        if (userInput.startsWith("mark ") && userInputNumber <= 8) {
+            number = userInput.substring(4).trim();
+        } else if (userInput.startsWith("unmark ") && userInputNumber <= 10) {
+            number = userInput.substring(6).trim();
+        }
+
+        return Integer.parseInt(number);
+
+    }
+
+    public static void displayUserDataArray(Task[] userDataArray, int numberOfItems) {
         drawHorizontalLine();
+        System.out.println("Here are the tasks in your list");
         for (int itemIndex = 0; itemIndex < numberOfItems; itemIndex += 1){
-            System.out.println("     " + (itemIndex + 1) + ". " + userDataArray[itemIndex]);
+            System.out.print("     " + (itemIndex + 1) + ".");
+            System.out.print("[" + userDataArray[itemIndex].getTaskStatusSymbol() + "] ");
+            System.out.println(userDataArray[itemIndex].taskName);
         }
         drawHorizontalLine();
         System.out.println();
     }
 
+    public static void updateTaskStatus(Task[] userDataArray, int taskNumber, String toDo) {
+        userDataArray[taskNumber - 1].setTaskStatus(toDo.equals("mark"));
+    }
+
     public static void echoUser() {
         String userInput;
-        String[] userDataArray = new String[100];
+        Task[] userDataArray = new Task[100];
         int numberOfItems = 0;
         Scanner in = new Scanner(System.in);
 
@@ -33,10 +53,29 @@ public class TaskHandler {
             userInput = in.nextLine();
             if (userInput.equals("bye")) {
                 break;
+
             } else if (userInput.equals("list")) {
                 displayUserDataArray(userDataArray, numberOfItems);
+
+            } else if (userInput.startsWith("mark ")) {
+                int number = Integer.parseInt(userInput.substring(5));
+                updateTaskStatus(userDataArray, number, "mark");
+                drawHorizontalLine();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  [X] " + userDataArray[number - 1].taskName);
+                drawHorizontalLine();
+
+            } else if (userInput.startsWith("unmark ")) {
+                int number = Integer.parseInt(userInput.substring(7));
+                updateTaskStatus(userDataArray, number, "unmark");
+                drawHorizontalLine();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  [ ] " + userDataArray[number - 1].taskName);
+                drawHorizontalLine();
+
             } else {
-                userDataArray[numberOfItems] = userInput;
+                Task aTask = new Task(userInput);
+                userDataArray[numberOfItems] = aTask;
                 numberOfItems = numberOfItems + 1;
                 drawHorizontalLine();
                 System.out.println("     added: " + userInput);
