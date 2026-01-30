09b96ccbfe46bba6b0953e350cfd1022f352d817
VinayVR26 <133363378+VinayVR26@users.noreply.github.com>
2026-01-30 10:30:24 +0800

    Completed Level-1

--- src/main/java/TaskHandler.java
+++ src/main/java/TaskHandler.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class TaskHandler {
 
-    public static void horizontalLines() {
+    public static void drawHorizontalLine() {
+        System.out.print("    ");
         for (int i = 0; i < 60; i += 1){
             System.out.print("-");
 
@@ -10,12 +13,32 @@ public class TaskHandler {
         }
     }
 
+    public static void echoUser() {
+        String userInput;
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("");
+        userInput = in.nextLine();
+
+        while (!(userInput.equals("bye"))) {
+            drawHorizontalLine();
+            System.out.println("    " + userInput);
+            drawHorizontalLine();
+            System.out.print("");
+            userInput = in.nextLine();
+        }
+
+        drawHorizontalLine();
+        System.out.println("    I hope I helped you! Bye for now");
+        drawHorizontalLine();
+    }
+
     public static void main(String[] args) {
-        horizontalLines();
-        System.out.println("Hello! I'm TaskHandler");
-        System.out.println("What can I do for you?");
-        horizontalLines();
-        System.out.println("Bye. Hope to see you again soon!");
-        horizontalLines();
+        drawHorizontalLine();
+        System.out.println("    Hello! I'm TaskHandler");
+        System.out.println("    Tell me what to do. I am happy to assist you.");
+        drawHorizontalLine();
+
+        echoUser();
     }
 }
