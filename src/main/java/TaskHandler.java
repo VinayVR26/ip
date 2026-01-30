e81a2b8818fb846d3644050baf652cb628dd4d40
VinayVR26 <133363378+VinayVR26@users.noreply.github.com>
2026-01-30 11:20:51 +0800

    Completed Level 2

--- src/main/java/TaskHandler.java
+++ src/main/java/TaskHandler.java
@@ -13,19 +13,36 @@ public class TaskHandler {
         }
     }
 
+    public static void displayUserDataArray(String[] userDataArray, int numberOfItems) {
+        drawHorizontalLine();
+        for (int itemIndex = 0; itemIndex < numberOfItems; itemIndex += 1){
+            System.out.println("     " + (itemIndex + 1) + ". " + userDataArray[itemIndex]);
+        }
+        drawHorizontalLine();
+        System.out.println();
+    }
+
     public static void echoUser() {
         String userInput;
+        String[] userDataArray = new String[100];
+        int numberOfItems = 0;
         Scanner in = new Scanner(System.in);
 
-        System.out.print("");
-        userInput = in.nextLine();
-
-        while (!(userInput.equals("bye"))) {
-            drawHorizontalLine();
-            System.out.println("    " + userInput);
-            drawHorizontalLine();
+        while (true) {
             System.out.print("");
             userInput = in.nextLine();
+            if (userInput.equals("bye")) {
+                break;
+            } else if (userInput.equals("list")) {
+                displayUserDataArray(userDataArray, numberOfItems);
+            } else {
+                userDataArray[numberOfItems] = userInput;
+                numberOfItems = numberOfItems + 1;
+                drawHorizontalLine();
+                System.out.println("     added: " + userInput);
+                drawHorizontalLine();
+                System.out.println();
+            }
         }
 
         drawHorizontalLine();
@@ -36,8 +53,9 @@ public class TaskHandler {
     public static void main(String[] args) {
         drawHorizontalLine();
         System.out.println("    Hello! I'm TaskHandler");
-        System.out.println("    Tell me what to do. I am happy to assist you.");
+        System.out.println("    Tell me what to do. I am happy to assist you.\n");
         drawHorizontalLine();
+        System.out.println();
 
         echoUser();
     }
